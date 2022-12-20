c33f2ba46e6406bb01ff3c2ece714fbbd16f5a68
w4445 <m19168@g.metro-cit.ac.jp>
2022-12-20 14:24:17 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+    "net/http"
+    "github.com/labstack/echo"
+)
+
+func main() {
+    e := echo.New()
+
+    e.GET("/hello", Hello())
+    e.GET("/api/hello", ApiHelloGet())
+
+    e.Start(":8080")
+}
+
+func Hello() echo.HandlerFunc {
+    return func(c echo.Context) error {     
+        return c.String(http.StatusOK, "4445:pentakill ver.2")
+    }
+}
+
+func ApiHelloGet() echo.HandlerFunc {
+    return func(c echo.Context) error {     
+        return c.JSON(http.StatusOK, map[string]interface{}{"m19168": "4445", "message": "leagens never die"})
+    }
+}
